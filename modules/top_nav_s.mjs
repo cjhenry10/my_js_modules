fd85f040c241582d7ff2be68d2f0a0fe54ecf617
Connor Henry <cjhenry10@gmail.com>
2022-04-18 01:34:21 -0400

    figured out the rest of the animation for the navbar closing

--- modules/top_nav_s.mjs
+++ modules/top_nav_s.mjs
@@ -1,7 +1,23 @@
 export function responsive() {
     const toggleButton = document.getElementsByClassName('toggle-button')[0];
-    const navLinks = document.getElementsByClassName('navbar-links')[0];
+    const navMenu = document.getElementsByClassName('navbar-links')[0];
+    const navLinks = document.getElementsByClassName('nav-item');
+    console.log(navLinks)
     toggleButton.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
+        if (navMenu.classList.contains('active')) {
+            navMenu.classList.add('closing');
+            setTimeout(() => {
+                navMenu.classList.remove('active');
+                navMenu.classList.remove('closing')
+            }, 500)
+        }
+        else if (navMenu.classList.contains('closing')) {
+            
+            navMenu.classList.remove('closing');
+            navMenu.classList.add('active');
+        }
+        else {
+            navMenu.classList.add('active');
+        }
     })
 }
